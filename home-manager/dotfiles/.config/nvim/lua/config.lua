e61ec42c686e8e56f31168609b2892119ade8c57
Cian Hughes <chughes000@gmail.com>
2024-04-15 17:56:14 +0100

    Forgot to add config.lua

---
+++ home-manager/dotfiles/.config/nvim/lua/config.lua
@@ -0,0 +1,40 @@
+-- First, i want to configure my vanilla nvim settings
+vim.wo.number = true -- Line numbers
+vim.wo.relativenumber = true
+vim.opt.wrap = false
+vim.opt.mouse = "a" -- enable mouse mode for window resizing
+vim.opt.clipboard = "unnamedplus" -- share system and nvim clipboard
+vim.g.have_nerd_font = true
+vim.opt.undofile = true -- Save undo history
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = "yes" -- Keep signcolumn on by default
+-- Decrease update time and timeout between key commands
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+-- Render markers so we don't mistakenly out tabs (and other whitespace) where we dont want them
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+-- Also, while we're at it: let's set the tab widths
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.inccommand = "split" -- Preview substitutions live, as you type!
+vim.opt.cursorline = true -- Show which line your cursor is on
+vim.opt.scrolloff = 10 -- Minimal number of screen lines to keep above and below the cursor.
+-- Set highlight on search, but clear on pressing <Esc> in normal mode
+vim.opt.hlsearch = true
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
