62cf6fe8b2f3db0c47a845f84a0391009ce5a810
Cian Hughes <chughes000@gmail.com>
2024-07-04 17:21:59 +0100

    Removed image.nvim
    This plugin is nice in my main work env, but is an endless source of
    errors in other envs. its nice sometimes but i dont get enough value for
    the headache to be worth it.

--- home-manager/dotfiles/.config/nvim/lua/plugins/ui.lua
+++ home-manager/dotfiles/.config/nvim/lua/plugins/ui.lua
@@ -55,7 +55,6 @@ return { -- UI components and other visual elements are declared here
 			"nvim-lua/plenary.nvim",
 			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 			"MunifTanjim/nui.nvim",
-			"3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 		},
 		config = function()
 			vim.keymap.set("n", "<leader>tt", function()
