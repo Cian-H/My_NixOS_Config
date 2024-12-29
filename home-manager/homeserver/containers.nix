f98fcc99bdd2c08a3438e46183f8f63834ebf1fc
Cian Hughes <chughes000@gmail.com>
2024-12-29 11:59:31 +0000

    Updated to enable home-manager podman service

--- home-manager/homeserver/containers.nix
+++ home-manager/homeserver/containers.nix
@@ -7,19 +7,24 @@
   unstablePkgs,
   ...
 }: {
-  services.podman.containers = {
-    portainer = {
-      image = "portainer/portainer-ce:latest";
-      volumes = [
-        "/var/run/docker.sock:/var/run/docker.sock"
-        "portainer_data:/data"
-      ];
-      ports = [
-        "8000:8000"
-        "9443:9443"
-      ];
-      autoStart = true;
-      autoUpdate = "registry";
+  services.podman = {
+    enable = true;
+    enableTypeChecks = true;
+    autoUpdate.enable = true;
+    containers = {
+      portainer = {
+        image = "portainer/portainer-ce:latest";
+        volumes = [
+          "/var/run/docker.sock:/var/run/docker.sock"
+          "portainer_data:/data"
+        ];
+        ports = [
+          "8000:8000"
+          "9443:9443"
+        ];
+        autoStart = true;
+        autoUpdate = "registry";
+      };
     };
   };
 }
