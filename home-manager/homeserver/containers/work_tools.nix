e80b2638fec26d2ce09f8455d4fb7468e91b3a11
Cian Hughes <chughes000@gmail.com>
2025-01-30 01:39:38 +0000

    Added freshrss container

--- home-manager/homeserver/containers/work_tools.nix
+++ home-manager/homeserver/containers/work_tools.nix
@@ -22,5 +22,21 @@
       ];
       ports = ["3456:3456"];
     };
+    freshrss = {
+      image = "docker.io/freshrss/freshrss:latest";
+      autoUpdate = "registry";
+      environment = {
+        TZ = "Europe/Dublin";
+        CRON_MIN = "1,31";
+      };
+      volumes = [
+        "/home/cianh/freshrss/data:/var/www/FreshRSS/data"
+        "/home/cianh/freshrss/extensions:/var/www/FreshRSS/extensions"
+      ];
+      ports = ["3457:80"];
+      extraPodmanArgs = [
+        "--log-opt max-size=10m"
+      ];
+    };
   };
 }
