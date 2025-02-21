522f5490bcdc7c5ad0280be31299a483bb9ec838
Cian Hughes <chughes000@gmail.com>
2025-02-21 09:58:56 +0000

    Updated gpu config

--- nixos/worklaptop/hardware-configuration.nix
+++ nixos/worklaptop/hardware-configuration.nix
@@ -53,5 +53,9 @@
   # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware = {
+    cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    graphics.enable = true;
+    nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 }
