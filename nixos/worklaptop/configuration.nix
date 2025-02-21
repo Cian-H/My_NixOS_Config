339df4d67684122e7b8b19c720a67e8ec085ff06
Cian Hughes <chughes000@gmail.com>
2025-02-21 15:39:43 +0000

    Added nvidia prime config

--- nixos/worklaptop/configuration.nix
+++ nixos/worklaptop/configuration.nix
@@ -23,6 +23,11 @@
     open = false;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
+    prime = {
+      sync.enable = true;
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
   };
   hardware.nvidia-container-toolkit.enable = true;
 
