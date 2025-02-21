f9a3062144489c97a66b95b625bc2c33cb1a4211
Cian Hughes <chughes000@gmail.com>
2025-02-21 09:59:06 +0000

    Patched slow obsidian boot

--- home-manager/worklaptop/packages.nix
+++ home-manager/worklaptop/packages.nix
@@ -76,11 +76,28 @@
     ])
     ++ (with unstablePkgs; [
       vimPlugins.mason-lspconfig-nvim
-      obsidian
+      (writeShellScriptBin "obsidian" ''        # Patch for obsiidan GPU issues
+               exec ${unstablePkgs.obsidian}/bin/obsidian --disable-gpu "$@"
+      '')
       zed-editor
       zotero
     ])
     ++ [
       inputs.zen-browser.packages.x86_64-linux.default
     ];
+
+  # manually add desktop file for patched obsidian
+  home.file.".local/share/applications/obsidian.desktop".text = ''
+    [Desktop Entry]
+    Name=Obsidian
+    Exec=${pkgs.writeShellScriptBin "obsidian-launcher" ''
+      exec ${pkgs.obsidian}/bin/obsidian --disable-gpu "$@"
+    ''}/bin/obsidian-launcher %U
+    Icon=${pkgs.obsidian}/share/icons/hicolor/512x512/apps/obsidian.png
+    Terminal=false
+    Type=Application
+    Categories=Office;
+    MimeType=x-scheme-handler/obsidian;
+    Keywords=obsidian;notes;
+  '';
 }
