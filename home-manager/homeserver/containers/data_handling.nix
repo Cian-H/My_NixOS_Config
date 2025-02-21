95af474b3afa97d04e43bf0fd4de89e627e4d5a9
Cian Hughes <chughes000@gmail.com>
2025-02-21 02:56:54 +0000

    Added grafana

--- home-manager/homeserver/containers/data_handling.nix
+++ home-manager/homeserver/containers/data_handling.nix
@@ -12,6 +12,10 @@
       image = "docker.io/grafana/grafana:latest";
       autoUpdate = "registry";
       ports = ["3000:3000"];
+      user = "1000:1000";
+      volumes = [
+        "/home/cianh/grafana:/var/lib/grafana:Z"
+      ];
     };
   };
 }
