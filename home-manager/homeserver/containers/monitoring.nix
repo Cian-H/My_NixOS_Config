d6f342a42177fbcaca18688be8eff83f204a83dc
Cian Hughes <chughes000@gmail.com>
2025-02-22 21:35:03 +0000

    Added prometheus monitoring to stack

--- home-manager/homeserver/containers/monitoring.nix
+++ home-manager/homeserver/containers/monitoring.nix
@@ -7,15 +7,32 @@
   unstablePkgs,
   ...
 }: {
-  services.podman.containers = {
-    grafana = {
-      image = "docker.io/grafana/grafana:latest";
-      autoUpdate = "registry";
-      ports = ["3000:3000"];
-      user = "1000:1000";
-      volumes = [
-        "/home/cianh/grafana:/var/lib/grafana:Z"
-      ];
+  services.podman = {
+    networks = {
+      grafana-net = {};
+    };
+    containers = {
+      grafana = {
+        image = "docker.io/grafana/grafana:latest";
+        autoUpdate = "registry";
+        network = ["grafana-net"];
+        ports = ["3000:3000"];
+        user = "1000:1000";
+        volumes = [
+          "/home/cianh/grafana:/var/lib/grafana:Z"
+        ];
+      };
+      prometheus = {
+        image = "docker.io/prom/prometheus:latest";
+        autoUpdate = "registry";
+        network = [
+          "grafana-net"
+          "proxy-net"
+        ];
+        volumes = [
+          "/home/cianh/prometheus/:/etc/prometheus/"
+        ];
+      };
     };
   };
 }
