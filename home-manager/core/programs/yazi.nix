2c21bb682896054c9f788ed2a2910b5937ddf5dc
Cian Hughes <chughes000@gmail.com>
2025-04-15 11:53:39 +0100

    Added plugins to yazi config

--- home-manager/core/programs/yazi.nix
+++ home-manager/core/programs/yazi.nix
@@ -9,18 +9,21 @@
   programs.yazi = {
     enable = true;
     package = unstablePkgs.yazi;
+    programs.yazi.enableNushellIntegration = true;
     initLua = ./yazi/init.lua;
     # plugins = {
-    #   sudo = ./yazi/plugins/sudo.yazi;
     #   chmod = ./yazi/plugins/core/chmod.yazi;
-    #   mime-ext = ./yazi/plugins/core/mime-ext.yazi;
-    #   git = ./yazi/plugins/core/git.yazi;
-    #   ouch = ./yazi/plugins/ouch.yazi;
-    #   starship = ./yazi/plugins/starship.yazi;
     #   full-border = ./yazi/plugins/core/full-border.yazi;
+    #   git = ./yazi/plugins/core/git.yazi;
     #   glow = ./yazi/plugins/glow.yazi;
     #   hexyl = ./yazi/plugins/hexyl.yazi;
     #   max-preview = ./yazi/plugins/core/max-preview.yazi;
+    #   mime-ext = ./yazi/plugins/core/mime-ext.yazi;
+    #   ouch = ./yazi/plugins/ouch.yazi;
+    #   rich-preview = ./yazi/plugins/core/rich-preview.yazi;
+    #   starship = ./yazi/plugins/starship.yazi;
+    #   sudo = ./yazi/plugins/sudo.yazi;
+    #   wl-clipboard = ./yazi/plugins/core/wl-clipboard.yazi;
     # };
     # flavors = {
     #   tokyo-night = ./yazi/flavors/tokyo-night.yazi;
@@ -34,16 +37,18 @@
 
   # Manually place plugins for now, until home-manager updates for newer yazi versions
   xdg.configFile = {
-    "yazi/plugins/sudo.yazi".source = ./yazi/plugins/sudo.yazi;
     "yazi/plugins/chmod.yazi".source = ./yazi/plugins/core/chmod.yazi;
-    "yazi/plugins/mime-ext.yazi".source = ./yazi/plugins/core/mime-ext.yazi;
-    "yazi/plugins/git.yazi".source = ./yazi/plugins/core/git.yazi;
-    "yazi/plugins/ouch.yazi".source = ./yazi/plugins/ouch.yazi;
-    "yazi/plugins/starship.yazi".source = ./yazi/plugins/starship.yazi;
     "yazi/plugins/full-border.yazi".source = ./yazi/plugins/core/full-border.yazi;
+    "yazi/plugins/git.yazi".source = ./yazi/plugins/core/git.yazi;
     "yazi/plugins/glow.yazi".source = ./yazi/plugins/glow.yazi;
     "yazi/plugins/hexyl.yazi".source = ./yazi/plugins/hexyl.yazi;
     "yazi/plugins/max-preview.yazi".source = ./yazi/plugins/core/max-preview.yazi;
+    "yazi/plugins/mime-ext.yazi".source = ./yazi/plugins/core/mime-ext.yazi;
+    "yazi/plugins/ouch.yazi".source = ./yazi/plugins/ouch.yazi;
+    "yazi/plugins/rich-preview.yazi".source = ./yazi/plugins/core/rich-preview.yazi;
+    "yazi/plugins/starship.yazi".source = ./yazi/plugins/starship.yazi;
+    "yazi/plugins/sudo.yazi".source = ./yazi/plugins/sudo.yazi;
+    "yazi/plugins/wl-clipboard.yazi".source = ./yazi/plugins/core/wl-clipboard.yazi;
     "yazi/flavors/tokyo-night.yazi".source = ./yazi/flavors/tokyo-night.yazi;
   };
 }
