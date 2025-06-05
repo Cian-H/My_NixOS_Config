9e4b1398988d7b03cd1e4816e7ea36229324b648
Cian Hughes <chughes000@gmail.com>
2025-06-05 10:18:42 +0100

    Suppressed podman compose warning

--- nixos/worklaptop/environment.nix
+++ nixos/worklaptop/environment.nix
@@ -11,6 +11,7 @@
       LIBVA_DRIVER_NAME = "iHD";
       NIXOS_OZONE_WL = "1";
       ELECTRON_OZONE_PLATFORM_HINT = "wayland";
+      PODMAN_COMPOSE_WARNING_LOGS = false;
     };
     etc =
       lib.mapAttrs'
