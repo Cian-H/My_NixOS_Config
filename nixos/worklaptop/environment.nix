1dac2a433eeddbe34d532f1d4300f08cee334066
Cian Hughes <chughes000@gmail.com>
2025-06-18 10:06:15 +0100

    changed type of podman compose warning suppression flag

--- nixos/worklaptop/environment.nix
+++ nixos/worklaptop/environment.nix
@@ -11,7 +11,7 @@
       LIBVA_DRIVER_NAME = "iHD";
       NIXOS_OZONE_WL = "1";
       ELECTRON_OZONE_PLATFORM_HINT = "wayland";
-      PODMAN_COMPOSE_WARNING_LOGS = false;
+      PODMAN_COMPOSE_WARNING_LOGS = "false";
     };
     etc =
       lib.mapAttrs'
