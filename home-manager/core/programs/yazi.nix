1e58131f67b5f457be839dca239f1513e6deb7a7
Cian Hughes <chughes000@gmail.com>
2026-06-02 10:54:06 +0100

    Further updates to yazi config

--- home-manager/core/programs/yazi.nix
+++ home-manager/core/programs/yazi.nix
@@ -19,16 +19,16 @@
     "yazi/theme.toml".source = ../dotfiles/dot_config/yazi/theme.toml;
     "yazi/yazi.toml".source = ../dotfiles/dot_config/yazi/yazi.toml;
     "yazi/plugins/chmod.yazi".source = ../dotfiles/dot_config/yazi/plugins/core/chmod.yazi;
+    "yazi/plugins/clipboard.yazi".source = ../dotfiles/dot_config/yazi/plugins/clipboard.yazi;
     "yazi/plugins/full-border.yazi".source = ../dotfiles/dot_config/yazi/plugins/core/full-border.yazi;
     "yazi/plugins/git.yazi".source = ../dotfiles/dot_config/yazi/plugins/core/git.yazi;
-    "yazi/plugins/hexyl.yazi".source = ../dotfiles/dot_config/yazi/plugins/hexyl.yazi;
     "yazi/plugins/max-preview.yazi".source = ../dotfiles/dot_config/yazi/plugins/max-preview.yazi;
     "yazi/plugins/mime-ext.yazi".source = ../dotfiles/dot_config/yazi/plugins/core/mime-ext.yazi;
     "yazi/plugins/ouch.yazi".source = ../dotfiles/dot_config/yazi/plugins/ouch.yazi;
+    "yazi/plugins/piper.yazi".source = ../dotfiles/dot_config/yazi/plugins/yazi-rs-plugins/piper.yazi;
     "yazi/plugins/rich-preview.yazi".source = ../dotfiles/dot_config/yazi/plugins/rich-preview.yazi;
     "yazi/plugins/starship.yazi".source = ../dotfiles/dot_config/yazi/plugins/starship.yazi;
     "yazi/plugins/sudo.yazi".source = ../dotfiles/dot_config/yazi/plugins/sudo.yazi;
-    "yazi/plugins/wl-clipboard.yazi".source = ../dotfiles/dot_config/yazi/plugins/wl-clipboard.yazi;
     "yazi/flavors/tokyo-night.yazi".source = ../dotfiles/dot_config/yazi/flavors/tokyo-night.yazi;
   };
 }
