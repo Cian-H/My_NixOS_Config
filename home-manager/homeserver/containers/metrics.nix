251ca3907eba4064504d891df6d44255571eb6a1
Cian Hughes <chughes000@gmail.com>
2026-06-23 01:07:03 +0100

    Attempt to fix metrics header

--- home-manager/homeserver/containers/metrics.nix
+++ home-manager/homeserver/containers/metrics.nix
@@ -11,7 +11,7 @@
     containers = {
       goaccess = {
         image = "docker.io/allinurl/goaccess:latest";
-        exec = "--output=/var/www/goaccess/index.html --log-file=/var/log/caddy/access.log --log-format=CADDY --tz=Europe/Dublin --anonymize-ip --real-time-html --ws-url=wss://metrics.bulba.space:443/ws --port=7890 --origin=metrics.bulba.space";
+        exec = "--output=/var/www/goaccess/index.html --log-file=/var/log/caddy/access.log --log-format=CADDY --tz=Europe/Dublin --anonymize-ip --real-time-html --ws-url=wss://metrics.bulba.space:443/ws --port=7890 --origin=https://metrics.bulba.space";
         autoUpdate = "registry";
         network = [
           "proxy-net"
