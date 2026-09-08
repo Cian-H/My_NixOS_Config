ef15f8006f9709dad1e63b4e96cc649ce277521e
Cian Hughes <chughes000@gmail.com>
2026-09-08 14:20:11 +0100

    fix: added bypass for wait-network-online behaviour

--- home-manager/worklaptop/services.nix
+++ home-manager/worklaptop/services.nix
@@ -26,23 +26,33 @@
     };
   };
   # Custom version of ghostty service, to stop closing during update
-  systemd.user.services."app-com.mitchellh.ghostty" = {
-    Unit = {
-      Description = "Ghostty Terminal Emulator";
-      X-SwitchMethod = "keep-old";
-      X-RestartIfChanged = false;
-      After = ["graphical-session.target"];
+  systemd.user.services = {
+    "app-com.mitchellh.ghostty" = {
+      Unit = {
+        Description = "Ghostty Terminal Emulator";
+        X-SwitchMethod = "keep-old";
+        X-RestartIfChanged = false;
+        After = ["graphical-session.target"];
+      };
+      Service = {
+        ExecStart = "${lib.getExe config.programs.ghostty.package}";
+        Environment = "WAYLAND_DISPLAY=wayland-1";
+        Type = "notify";
+        ReloadSignal = "SIGUSR2";
+        KillMode = "mixed";
+        Restart = "on-failure";
+      };
+      Install = {
+        WantedBy = ["graphical-session.target"];
+      };
     };
-    Service = {
-      ExecStart = "${lib.getExe config.programs.ghostty.package}";
-      Environment = "WAYLAND_DISPLAY=wayland-1";
-      Type = "notify";
-      ReloadSignal = "SIGUSR2";
-      KillMode = "mixed";
-      Restart = "on-failure";
-    };
-    Install = {
-      WantedBy = ["graphical-session.target"];
+    "podman-user-wait-network-online" = {
+      Service = {
+        ExecStart = [
+          ""
+          "${pkgs.coreutils}/bin/true"
+        ];
+      };
     };
   };
 }
