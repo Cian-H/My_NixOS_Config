d18c8b74c1f389ccd3856555fcbdac15edb0576d
Cian Hughes <chughes000@gmail.com>
2026-09-08 13:57:04 +0100

    fix: enabled podman startup service

--- home-manager/worklaptop/services.nix
+++ home-manager/worklaptop/services.nix
@@ -9,15 +9,20 @@
 }: {
   services = {
     pueue.enable = true;
-    podman.containers."9router" = {
-      image = "decolua/9router:latest";
-      autoStart = true;
-      ports = [
-        "20128:20128"
-      ];
-      volumes = [
-        "${config.home.homeDirectory}/.config/9router:/data"
-      ];
+    podman = {
+      enable = true;
+      enableTypeChecks = true;
+      autoUpdate.enable = true;
+      containers."9router" = {
+        image = "decolua/9router:latest";
+        autoStart = true;
+        ports = [
+          "20128:20128"
+        ];
+        volumes = [
+          "${config.home.homeDirectory}/.config/9router:/data"
+        ];
+      };
     };
   };
   # Custom version of ghostty service, to stop closing during update
